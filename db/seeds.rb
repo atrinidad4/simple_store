46b4193acf17d6018b2575d261f57e7db901c6f5
Alyssa Trinidad <atrinidad4@rrc.com>
2024-10-21 14:36:51 -0500

    updated seeds file to seed products and categories from CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -8,11 +9,26 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-676.times do
+# clears the existing data
+Product.destroy_all
+Category.destroy_all
+
+# parse CSV and seed data
+csv_file = Rails.root.join('db', 'products.csv')
+csv_data = CSV.read(csv_file, headers: true)
+
+csv_data.each do |row|
+  # creates or finds category
+  category = Category.find_or_create_by(name: row['category'])
+
+  # creates the product
   Product.create(
-    title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph,
-    price: Faker::Commerce.price,
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
+    title: row['title'],
+    description: row['description'],
+    price: row['price'],
+    stock_quantity: row['stock_quantity'],
+    category: category
   )
 end
+
+puts "Seeding complete!"
