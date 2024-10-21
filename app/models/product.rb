4f70f7f217200290b45b42a61857123ba8bf2cd1
Alyssa Trinidad <atrinidad4@rrc.com>
2024-10-21 14:31:58 -0500

    i added associations between the product and category models

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,3 +3,8 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :stock_quantity, presence: true
 end
+
+class Product < ApplicationRecord
+  belongs_to :category
+  validates :title, :price, :stock_quantity, presence: true
+end
